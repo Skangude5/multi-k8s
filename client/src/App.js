50c6b69597d056d66a51b621d9249368e0bb278f
Sharad Kangude <skangude5@icloud.com>
2023-08-23 21:35:12 +0530

    added cert

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            FIB calculator KUBERNETES!!
+            FIB calculator KUBERNETES With HTTPS!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
